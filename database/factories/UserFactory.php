202ff8eabbfea799850c872a81a78c5dbdb443e0
Safghan Alam <khansafgan1743@gmail.com>
2025-03-07 02:52:24 +0530

    Make store and user relationship

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\Store;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -24,10 +25,20 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'username' => fake()->username(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => "password",
+            'phone' => fake()->phoneNumber(),
+            'address_1' => fake()->address(),
+            'address_2' => fake()->streetAddress(),
+            'city' => fake()->city(),
+            'state'=>fake()->country(),
+            'country'=>fake()->country(),
+            'zip'=>fake()->postcode(),
+            'avatar' => fake()->image(),
+            'admin' => true,
             'remember_token' => Str::random(10),
         ];
     }
